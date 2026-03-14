10b68622dcc776f954e3cb3e85ad3b912b8c800b
huasha765-good <huasha765@gmail.com>
2026-03-14 21:53:16 +0800

    重构路由：切换为 History 模式并增加首页重定向

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+// 修改点1：换成 BrowserRouter，并引入 Navigate 组件用于重定向
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { navItems } from "./nav-items";
 
 const queryClient = new QueryClient();
@@ -10,13 +11,16 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <HashRouter>
+      {/* 修改点2：使用 BrowserRouter (去掉 URL 里的 # 号) */}
+      <BrowserRouter>
         <Routes>
           {navItems.map(({ to, page }) => (
             <Route key={to} path={to} element={page} />
           ))}
+          {/* 修改点3：新增“自动导航”。如果访问根路径 /，自动跳转到 navItems 里的第一个页面 */}
+          <Route path="/" element={<Navigate to={navItems[0].to} replace />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
