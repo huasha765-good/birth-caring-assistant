c7c1f4a20d8b17893e5a8925d84dfc35f9b891d8
huasha765-good <huasha765@gmail.com>
2026-03-14 22:03:23 +0800

    修复路由死循环：移除多余的 Navigate 重定向

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,12 @@ const App = () => (
       <Toaster />
       {/* 修改点2：使用 BrowserRouter (去掉 URL 里的 # 号) */}
       <BrowserRouter>
-<Routes>
-  {navItems.map(({ to, page }) => (
-    <Route key={to} path={to} element={page} />
-  ))}
-  {/* 删掉那行 Navigate，让路由直接匹配 navItems 里的 "/" */}
-</Routes>
+        <Routes>
+         {navItems.map(({ to, page }) => (
+         <Route key={to} path={to} element={page} />
+         ))}
+         {/* 删掉那行 Navigate，让路由直接匹配 navItems 里的 "/" */}
+       </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
